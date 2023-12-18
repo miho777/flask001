322cc834c92f47046f5409ffb7ce01c69c19f5a2
miho777 <naozumi.miho@emc.com>
2023-12-18 23:27:35 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+##### Create a Flask instance
+app = Flask(__name__)
+count = 0
+##### Define routes #####
+@app.route('/')
+def home(): 
+    global count
+    count = count + 1
+    RET_TEXT = 'Hello, World! ' + str(count)
+    return RET_TEXT
+    
+##### Run the Flask instance, browse to http://<< Host IP or URL >>:5000 #####
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port='5000')
