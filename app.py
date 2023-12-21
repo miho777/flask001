214ebc4d18c72d659c06819069879ee3a6e2a919
miho777 <naozumi.miho@emc.com>
2023-12-21 10:03:44 +0900

    large font

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ def home():
     global count
     count = count + 1
     RET_TEXT = 'Hello World! count= ' + str(count)
-    return RET_TEXT
-    
+    # return RET_TEXT
+    return f'<html><head><style>body {{font-size: 32px;}}</style></head><body><p>{RET_TEXT}</p></body></html>'
+
 ##### Run the Flask instance, browse to http://<< Host IP or URL >>:8080 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port='8080')
